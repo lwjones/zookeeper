cfb37312e7ba70e90b794bf027707c6fec3ed53b
Lawrence Jones <git@lwjon.es>
2022-03-01 15:06:52 -0800

    Add new animals

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
+const fs = require('fs');
+const path = require('path');
 const PORT = process.env.PORT || 3001;
 const express = require('express');
 const app = express();
+// Parses incoming POST data into key/values pairs via `req.body`
+// `extended` checks for nested / sub-array data
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());  // parse incoming JSON
 
 const { animals } = require('./data/animals');
 
@@ -56,6 +62,35 @@ function findById(id, animalsArray) {
 }
 
 
+function createNewAnimal(body, animalsArray) {
+  const animal = body;
+
+  animalsArray.push(animal);
+  fs.writeFileSync(
+    path.join(__dirname, './data/animals.json'),
+    JSON.stringify({ animals: animalsArray }, null, 2)
+  );
+
+  return animal;
+}
+
+
+function validateAnimal(animal) {
+  if (!animal.name || typeof animal.name !== 'string') {
+    return false;
+  }
+  if (!animal.species || typeof animal.species !== 'string') {
+    return false;
+  }
+  if (!animal.diet || typeof animal.diet !== 'string') {
+    return false;
+  }
+  if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+    return false;
+  }
+  return true;
+}
+
 app.get('/api/animals', (req, res) => {
   let results = animals;
   if (req.query) {
@@ -69,6 +104,21 @@ app.get('/api/animals/:id', (req, res) => {
   const result = findById(req.params.id, animals);
   res.json(result);
 })
+
+
+app.post('/api/animals', (req, res) => {
+  // set id based on what the next index of the array will be
+  req.body.id = animals.length.toString();
+
+  // if any data in req.body is incorrect, send 400 error back
+  if (!validateAnimal(req.body)) {
+    res.status(400).send('The animal is not properly formatted.');
+  } else {
+    // add animal to json file and animals array in this function
+    const animal = createAnimal(req.body, animals);
+    res.json(animal)
+  }
+});
 
 
 app.listen(PORT, () => {
